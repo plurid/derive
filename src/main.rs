512cb82c99d0a65a4ef1c2994274ca6bdc57c4c6
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-03-06 08:46:38 +0200

    dev: init

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate clap;
 
+
+use std::env;
 use clap::{App, Arg, SubCommand};
 
 
@@ -24,10 +26,33 @@ fn main() {
 
     let matches = app.get_matches();
 
+    let mut init_directory = "";
+    if let Some(matches) = matches.subcommand_matches("init") {
+        if let Some(matches) = matches.value_of("input") {
+            init_directory = matches;
+        }
+    }
+
     match matches.subcommand_name() {
-        Some("init") => println!("'dev init' was run."),
-        Some("save") => println!("'dev save' was run."),
+        Some("init") => init(init_directory),
+        Some("save") => save(),
         None         => println!("No subcommand was used"),
         _            => println!("Some other subcommand was used"),
     }
+}
+
+
+fn init(directory: &str) {
+    println!("'dev init' was run.");
+    if directory.is_empty() {
+        let path = env::current_dir().unwrap();
+        println!("{}", path.display());
+    } else {
+        println!("{}", directory);
+    }
+}
+
+
+fn save() {
+    println!("'dev save' was run.");
 }
